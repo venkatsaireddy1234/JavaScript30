9f9d46f114c84d09484d8034df182f58f7b182eb
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-11-03 12:12:05 +0530

    Added drag and drop

--- Practice/KanbanBoard/JS/script1.js
+++ Practice/KanbanBoard/JS/script1.js
@@ -27,7 +27,7 @@ const closeModal = () => {
 //Adding Ticket
 const addTicket = document.querySelector(".add-card");
 const ticketContent = document.querySelector(".modal-textarea");
-const todoContainer = document.querySelector("#todo");
+const todoContainer = document.getElementById("todo");
 let colorPriority = "red";
 let ticketStatus = "todo";
 let ticketArray = [];
@@ -41,42 +41,50 @@ addTicket.addEventListener("click", () => {
   handleAddTicket();
 });
 const handleAddTicket = () => {
-  console.log("hi");
   let ticketText = ticketContent.value.trim();
-  const ticketId = new ShortUniqueId().randomUUID();
   if (ticketText.length) {
-    const ticketElem = createTicket(ticketId, ticketText, colorPriority);
-    toggleModal();
-    ticketContent.value = "";
-    ticketArray.push({
-      id: ticketId,
-      ticketCont: ticketText,
-      status: ticketStatus,
-      color: colorPriority,
-    });
-    todoContainer.appendChild(ticketElem);
-    updateLocalStorage();
+    createTicket(ticketText);
   }
 };
-const createTicket = (id, text, color) => {
+
+const createTicket = (ticketText) => {
+  const ticketId = new ShortUniqueId().randomUUID();
+  const ticketElem = createTicketEle(ticketId, ticketText, colorPriority);
+  todoContainer.appendChild(ticketElem);
+  toggleModal();
+  clearTextArea();
+  ticketArray.push({
+    id: ticketId,
+    ticketCont: ticketText,
+    status: ticketStatus,
+    color: colorPriority,
+  });
+  updateLocalStorage();
+  handleEditTicket(ticketElem, ticketId);
+};
+function createTicketEle(id, text, color) {
   let ticket = document.createElement("div");
   ticket.className = "ticket-cont";
   ticket.innerHTML = `
     <div class="ticket-color ${color ? color : ""}"></div>
-    <div class="ticket-id">#${id}</div>
+    <div class="ticket-id">${id}</div>
     <div class="ticket-task">${text}</div>
-    <div class="lock-unlock">
-        <i class="fa-solid fa-lock"></i>
-    </div>
+    <button class="lock-unlock">
+      <i class="fa-solid fa-lock">Locked</i>
+    </button>
     `;
   ticket.draggable = true;
   ticket.id = id;
   return ticket;
-};
+}
 const updateLocalStorage = () => {
   let stringifiedTicketData = JSON.stringify(ticketArray);
   localStorage.setItem("tickets", stringifiedTicketData);
 };
+
+function clearTextArea() {
+  ticketContent.value = "";
+}
 const handleGetColorName = (color) => {
   deActivateColorStyle(colors);
   activateColorStyle(color);
@@ -96,7 +104,7 @@ const activateColorStyle = (color) => {
 const inProgress = document.querySelector("#inProgress");
 const completed = document.querySelector("#completed");
 const appendTicketElem = (id, text, color, status) => {
-  const ticketCont = createTicket(id, color, color);
+  const ticketCont = createTicketEle(id, text, color);
   if (status === "todo") {
     todoContainer.appendChild(ticketCont);
   } else if (status === "inProgress") {
@@ -104,40 +112,177 @@ const appendTicketElem = (id, text, color, status) => {
   } else {
     completed.appendChild(ticketCont);
   }
+  handleEditTicket(ticketCont, id);
 };
 const loadTicketsFromLocalStorage = () => {
   const storedTickets = localStorage.getItem("tickets");
-  if (storedTickets.length) {
-    let ticketArr = JSON.parse(storedTickets);
-    for (let ticket of ticketArr) {
-      appendTicketElem(ticket.id, ticket.text, ticket.color, ticket.status);
+  if (storedTickets) {
+    let ticketArray = JSON.parse(storedTickets);
+    if (ticketArray.length) {
+      for (let ticket of ticketArray) {
+        appendTicketElem(
+          ticket.id,
+          ticket.ticketCont,
+          ticket.color,
+          ticket.status
+        );
+      }
     }
+  } else {
+    return;
   }
 };
 loadTicketsFromLocalStorage();
 
 //Delete the tickets
-const deleteBtn = document.querySelector(".remove-btn");
+let deleteBtn = document.querySelector(".remove-btn");
 let color = false;
+
+// Define an array to store references to ticket elements
+let tickets = document.querySelectorAll(".ticket-cont");
+
 deleteBtn.addEventListener("click", () => {
-  handleDeleteButton();
+  handleRemoveBtn();
 });
-const handleDeleteButton = () => {
-  if (color) {
-    deleteBtn.style.color = "inherit";
-    color = !color;
-  } else {
-    window.alert("Delete Button has been activated");
+
+function handleRemoveBtn() {
+  if (!color) {
+    window.alert("Delete button has been activated");
     deleteBtn.style.color = "red";
-    const tickets = document.querySelectorAll(".ticket-cont");
+
+    // Add event listeners to each ticket for removal
     tickets.forEach((ticket) => {
       ticket.addEventListener("click", () => {
-        if (color) {
-          const removedTicket = ticket.remove();
-          localStorage.removeItem(removedTicket);
+        if (color === true) {
+          // Get the ticket ID and remove the ticket element
+          const ticketId = ticket.id;
+          ticket.remove();
+
+          // Remove the corresponding entry from localStorage
+          removeTicketFromLocalStorage(ticketId);
         }
       });
     });
-    color = !color;
+
+    color = true;
+  } else {
+    color = false;
+    deleteBtn.style.color = "inherit";
+
   }
-};
+}
+
+function removeTicketFromLocalStorage(ticketId) {
+  // Get the stored tickets from localStorage
+  const storedTickets = localStorage.getItem("tickets");
+  if (storedTickets) {
+    const ticketArray = JSON.parse(storedTickets);
+
+    // Find the index of the ticket with the matching ID
+    const idx = ticketArray.findIndex((ticket) => ticket.id === ticketId);
+    console.log(idx);
+    if (idx !== -1) {
+      // Remove the ticket entry from the array
+      ticketArray.splice(idx, 1);
+
+      // Update localStorage with the modified ticket array
+      localStorage.setItem("tickets", JSON.stringify(ticketArray));
+    }
+  }
+}
+
+//Filter colors
+const colour = document.querySelectorAll(".color");
+colour.forEach((clr) => {
+  clr.addEventListener("click", () => {
+    let selectedClr = clr.classList[1];
+    const tickets = document.querySelectorAll(".ticket-color");
+
+    tickets.forEach((ticket) => {
+      let currentTicketColor = ticket.classList[1];
+      if (selectedClr === "all") {
+        ticket.parentElement.style.display = "block";
+      } else {
+        let displayStyle =
+          currentTicketColor === selectedClr ? "block" : "none";
+        ticket.parentElement.style.display = displayStyle;
+      }
+    });
+  });
+});
+
+//Edit the Ticket
+function handleEditTicket(ticketElem, ticketId) {
+  const unLockSymbol = ticketElem.querySelector(".lock-unlock i");
+  const ticketTxt = ticketElem.querySelector(".ticket-task");
+  let toggle = true;
+
+  unLockSymbol.addEventListener("click", () => {
+    if (toggle) {
+      unLockSymbol.innerText = "Unlocked";
+      ticketTxt.contentEditable = true;
+      ticketTxt.focus();
+
+      toggle = !toggle;
+    } else {
+      unLockSymbol.innerText = "Locked";
+      ticketTxt.contentEditable = false;
+
+      const idx = ticketArray.findIndex((obj) => obj.id === ticketId);
+      ticketArray[idx].ticketCont = ticketTxt.innerText;
+      updateLocalStorage();
+      toggle = !toggle;
+    }
+  });
+}
+
+//Drag and Drop
+const mainCont = document.querySelector('.main-cont');
+
+mainCont.addEventListener('dragstart', handleTicketDragStart);
+mainCont.addEventListener('dragend', handleTicketDragEnd);
+mainCont.addEventListener('dragover', handleTicketDragOver);
+mainCont.addEventListener('drop', handleDrop);
+
+function handleTicketDragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
+  event.target.classList.add("dragging");
+}
+
+function handleTicketDragEnd(event) {
+  event.target.classList.remove("dragging");
+}
+
+function handleTicketDragOver(event) {
+  event.preventDefault();
+}
+
+function handleDrop(event) {
+  event.preventDefault();
+
+  const ticketId = event.dataTransfer.getData("text/plain");
+  const droppedTicket = document.getElementById(ticketId);
+
+  if (droppedTicket) {
+    const targetContainer = event.target.closest(".ticket-container");
+
+    if (targetContainer) {
+      const newStatus = targetContainer.id;
+
+      // Remove ticket from the source container
+      const sourceContainer = droppedTicket.closest(".ticket-container");
+      sourceContainer.removeChild(droppedTicket);
+
+      // Add the ticket to the target container
+      targetContainer.appendChild(droppedTicket);
+
+      // Update the ticket's status in your data
+      const idx = ticketArray.findIndex((obj) => obj.id === ticketId);
+
+      if (idx !== -1) {
+        ticketArray[idx].status = newStatus;
+        updateLocalStorage();
+      }
+    }
+  }
+}
